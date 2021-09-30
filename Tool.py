96a192f8ecb621eaf474df516d30048e7f7807f6
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-09-30 14:37:03 +0300

    Create Tool.py

---
+++ Tool.py
@@ -0,0 +1,20 @@
+import time
+
+
+t1 = time.time()
+
+
+x = "𓀽"
+x1 = "𓀽𓀽" 
+x2 = "𓀽𓀽𓀽"
+x3 = "𓀽𓀽𓀽𓀽"
+
+
+print("𓀽")
+print("𓀽𓀽")
+print("Viavaldi:Four Seasons")
+print("Dream Theater")
+
+t2 = time.time()
+t = t2 - t1
+print("Elapsed time is : ", t, " seconds")
